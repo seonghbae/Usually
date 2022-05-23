a29b153861ceb048b154add9aa6e3ad6dea428ed
[SW2/서울/1반] 이주연 <its.about2weeks@gmail.com>
2022-05-23 19:03:10 +0900

    feat: 고침 userAPI 라우터와 서비스에 성별 넣음

--- src/db/schemas/user-schema.js
+++ src/db/schemas/user-schema.js
@@ -36,8 +36,8 @@ const UserSchema = new Schema(
       required: false,
       default: 'basic-user',
     },
-    gender:{
-        type:String,
+    gender: {
+        type: String,
         required: true,
     },
   },
--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -23,7 +23,7 @@ userRouter.post('/register', async (req, res, next) => {
     const password = req.body.password;
     const gender = req.body.gender;
 
-    // 위 데이터를 유저 db에 추가하기
+       // 위 데이터를 유저 db에 추가하기
     const newUser = await userService.addUser({
       fullName,
       email,
