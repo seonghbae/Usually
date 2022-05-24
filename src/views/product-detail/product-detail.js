78925c6c6033f580dbba2266ca9850dfe900cbda
seonghbae <fullbloomh3ck@gmail.com>
2022-05-25 05:30:33 +0900

    [변경] 상품 상세 페이지 html 및 css 구현, js 및 로직 미구현

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -2,8 +2,13 @@ import * as Api from '/api.js';
 import { addCommas } from '/useful-functions.js';
 
 // 요소(element), input 혹은 상수
-const productItemContainer = document.querySelector('#productItemContainer');
-const button = ""; // 카테고리나 홈에서 사진 등
+const image = document.querySelector('#image');
+const madeBy = document.querySelector('#madeBy');
+const name = document.querySelector('#name');
+const price = document.querySelector('#price');
+const inventoryButton = document.querySelector('#inventoryButton');
+const buyButton = document.querySelector('#buyButton');
+// const button = ""; // 카테고리나 홈에서 사진 등
 
 addAllElements();
 addAllEvents();
@@ -13,39 +18,11 @@ async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  button.addEventListener('click', showProductList);
+  // button.addEventListener('click', showProductList);
 }
 
 // 회원가입 진행
-async function showProductList() {
-  const res = await fetch('/product');
-  const datas = await res.json();
-
-  datas.forEach((data) => {
-    const productItem = document.createElement("div");
-    productItem.classList.append("message media product-item");
-    productItem.setAttribute("id", `${data.shortId}`);
-    productItem.innerHTML = 
-    `<div class="message media product-item" id="${data.shortId}">
-        <div class="media-left">
-            <figure class="image">
-                <img src="${data.img}" alt="제품 이미지">
-            </figure>
-        </div>
-        <div class="media-content">
-            <div class="content">
-                <p class="name">${data.name}</p>
-                <p class="description">${data.description}</p>
-                <p class="price">${addCommas(data.price)}원</p>
-            </div>
-        </div>
-    </div>`;
-    // productItem.addEventListener("click");
-
-    productItemContainer.innerHTML += productItem;
-    
-  });
-
+async function showProductDetail(productId) {
 //   // 로그인 api 요청
 //   try {
 //     const data = { email, password };
@@ -67,4 +44,17 @@ async function showProductList() {
 //     console.error(err.stack);
 //     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
 //   }
+
+  const res = await fetch(`/product/${productId}`);
+  const data = await res.json();
+
+  image.src = data.img;
+  madeBy.innerHTML = data.madeBy;
+  name.innerHTML = data.name;
+  price.innerHTML = data.price;
+
+  // inventoryButton.addEventListener("click", );
+  // buyButton.addEventListener("click", );
 }
+
+export { showProductDetail }
