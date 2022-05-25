636181df0fc3f3bdc4619b9b06446864d97b3ab1
[SW2/서울/1반] 이주연 <its.about2weeks@gmail.com>
2022-05-25 17:51:27 +0900

    feat:추가 회원 삭제 user-services.js 함수 추가

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -129,6 +129,43 @@ class UserService {
 
     return user;
   }
+
+  async deleteUser(userInfoRequired){
+
+     // 객체 destructuring
+     const { userId, currentPassword } = userInfoRequired;
+
+     // 우선 해당 id의 유저가 db에 있는지 확인
+     let user = await this.userModel.findById(userId);
+ 
+     // db에서 찾지 못한 경우, 에러 메시지 반환
+     if (!user) {
+       throw new Error('가입 내역이 없습니다. 다시 한 번 확인해 주세요.');
+     }
+ 
+     // 이제, 정보 수정을 위해 사용자가 입력한 비밀번호가 올바른 값인지 확인해야 함
+ 
+     // 비밀번호 일치 여부 확인
+     const correctPasswordHash = user.password;
+     const isPasswordCorrect = await bcrypt.compare(
+       currentPassword,
+       correctPasswordHash
+     );
+ 
+     if (!isPasswordCorrect) {
+       throw new Error(
+         '현재 비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.'
+       );
+     }
+ 
+     // 유저 삭제 시작
+     user = await this.userModel.deleteOne({
+       userId,
+     });
+ 
+     return user;
+
+  }
 }
 
 const userService = new UserService(userModel);
