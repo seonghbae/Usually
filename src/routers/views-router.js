c85a8dc7955f38c4357e07b9e82a73d84952b9b9
노호연 <croookid@gmail.com>
2022-05-27 00:53:43 +0900

    [추가] 상품 판매 페이지 html/css 추가

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -12,6 +12,7 @@ viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/modify', serveStatic('modify'));
 viewsRouter.use('/my-page', serveStatic('my-page'));
+viewsRouter.use('/product-sell', serveStatic('product-sell'));
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
 
