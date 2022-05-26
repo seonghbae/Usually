ecdf56159b5da40c0081484add8a8efce34458d8
노호연 <croookid@gmail.com>
2022-05-26 16:44:10 +0900

    [추가]main페이지 상품 이미지 요소 추가 테스트 함수 완성

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,3 +1,5 @@
+import { addCommas } from '/useful-functions.js';
+
 // 이미지 슬라이더 구현
 let currentSlide = 1;
 let nextTranslate = 0;
@@ -77,21 +79,94 @@ const slideNext = function (){
 // Interval 설정
 let slideTimer = setInterval(slideNext, 5000);
 
-// 컨테이너에 mouseenter/mouseleave 을 통한 Interval 설정, 버튼 opacity 설정
-container.addEventListener('mouseenter', function(){
-    clearInterval(slideTimer);
-    sliderButton.forEach((e)=>{
-      e.style.opacity = 1;
-    })
-})
 
-container.addEventListener('mouseleave', function(){
-    slideTimer = setInterval(slideNext, 5000);
-    sliderButton.forEach((e)=>{
-      e.style.opacity = 0;
-    })
-})
+// main 내 nav 버튼 클릭시 상품 나열
+// 요소(element), input 혹은 상수
+const categoryProductItemContainer = document.querySelector('.main-category');
+const ageProductItemContainer = document.querySelector('.main-age');
+// main 내 nav버튼 노드들
+const categoryMenus = document.querySelectorAll('.main-navmenu-category')
+const ageMenus = document.querySelectorAll('.main-navmenu-age')
 
-// 이전 / 다음 버튼에 클릭 이벤트리스너 설정
-document.querySelector('.slider-prev-button').addEventListener('click', slidePrev);
-document.querySelector('.slider-next-button').addEventListener('click', slideNext);
+addAllElements();
+addAllEvents();
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllElements() {
+    addProductInfo(categoryMenus, categoryProductItemContainer);
+    addProductInfo(ageMenus, ageProductItemContainer);
+}
+
+// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllEvents() {
+    // 슬라이더 왼쪽, 오른쪽 버튼 리스너
+    document.querySelector('.slider-prev-button').addEventListener('click', slidePrev);
+    document.querySelector('.slider-next-button').addEventListener('click', slideNext);
+    // 컨테이너에 mouseenter/mouseleave 을 통한 Interval 설정, 버튼 opacity 설정
+    container.addEventListener('mouseenter', function(){
+        clearInterval(slideTimer);
+        sliderButton.forEach((e)=>{
+        e.style.opacity = 1;
+        })
+    })
+
+    container.addEventListener('mouseleave', function(){
+        slideTimer = setInterval(slideNext, 5000);
+        sliderButton.forEach((e)=>{
+        e.style.opacity = 0;
+        })
+    })
+}
+
+// 입력으로 들어오는 주소로 이동
+function newPage(productId) {
+    window.location.href = `/product/${productId}`;
+}
+
+// 상품 목록을 받아와 html을 뿌려주는 함수
+async function showProductList(productItemContainer) {
+    // 테스트 데이터
+    const data = {
+        shortId: '1',
+        img: '/bracelet8.jpg',
+        madeBy: 'company',
+        name: 'The Loop',
+        price: 20000,
+    };
+
+    const datas = [];
+    for (let i = 0; i < 3; i++) datas.push(data);
+    productItemContainer.innerHTML = '';
+    datas.forEach((data, index, array) =>{
+        const productItem = document.createElement('div');
+        productItem.classList.add('main-product-container');
+        productItem.innerHTML = `
+                <div class="main-product-image-container">
+                    <img class="main-product-image" src="${data.img}" alt="">
+                </div>
+                <div class="main-product-name">
+                    ${data.name}
+                </div>
+                <div class="main-product-price">
+                    ${addCommas(data.price)+'원'}
+                </div>
+        `;
+        // 상품 클릭시 해당 상품 상세 페이지로 이동
+        productItem.addEventListener('click', () => newPage(data.shortId));
+        productItemContainer.appendChild(productItem);
+    })
+    
+}
+async function addProductInfo(menus, productItemContainer){
+    for (let i = 0; i < menus.length; i++){
+        menus[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            for (let j = 0; j < menus.length; j++){
+                menus[j].classList.remove('main-navmenu-clicked');
+            }
+            menus[i].classList.add('main-navmenu-clicked');
+            showProductList(productItemContainer);
+        })
+    }
+}
+
