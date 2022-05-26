e15acf30a30d2b1c9e914f2ce05b324369843d51
[SW2/서울/1반] 이주연 <its.about2weeks@gmail.com>
2022-05-26 17:47:27 +0900

    [추가] order-schema 생성

---
+++ src/db/schemas/order-schema.js
@@ -0,0 +1,52 @@
+import { Schema } from 'mongoose';
+import { shortId } from './types/short-id';
+
+const OrderSchema = new Schema(
+    {
+        shortId,
+        user:{
+            type:String,
+            ref:'User',
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: false,
+        },
+        address: {
+            type: new Schema(
+                {
+                    postalCode: String,
+                    address1: String,
+                    address2: String,
+                },
+                {
+                    _id: false,
+                }
+            ),
+            required: false,
+        },
+        message: { //배송시 요청 사항
+            type:String,
+            default: "배송 전 연락 주세요",
+        },
+        products:[{ 
+            type: new Schema({
+                itemId:String,      //itemId에 product.shortId로 넣는다 
+                itemName: String,   //아이템 이름
+                qty:Number,         //주문한 아이템의 양
+                price:Number,       //각각 가격
+            })
+        }],
+        totalPrice:{
+            type:Number,
+            required:true,
+        }
+  },
+  {
+    collection: 'orders',
+    timestamps: true,
+  }
+);
+
+export { OrderSchema };
