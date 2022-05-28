8577f7f9a000adb71b9f459fb05b9c2ce919f251
seonghbae <fullbloomh3ck@gmail.com>
2022-05-29 00:56:23 +0900

    [변경] 전체 선택 체크박스 구현

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -2,7 +2,8 @@ import * as Api from '/api.js';
 import { addCommas } from '/useful-functions.js';
 
 // 요소(element), input 혹은 상수
-const productItemContainer = document.querySelector('#productItemContainer');
+const cartProductsContainer = document.querySelector('#cartProductsContainer');
+const selectAllCheckbox = document.querySelector('#selectAllCheckbox');
 
 const purchaseButton = document.querySelector('#purchaseButton');
 
@@ -11,23 +12,24 @@ addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllElements() {
-    showCartList();
+    // showCartList();
 }
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {}
+function addAllEvents() {
+    selectAllCheckbox.addEventListener('change', checkAll);
+}
+
+function checkAll() {
+    const checkboxes = document.querySelectorAll('.productCheckbox');
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = selectAllCheckbox.checked;
+    });
+}
 
 // 입력으로 들어오는 주소로 이동
 function newPage(productId) {
     window.location.href = `/product/${productId}`;
 }
 
-// 장바구니 목록
-async function showCartList() {
-    const items = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const item = localStorage.getItem(key);
-        items.push(item);
-    }
-}
